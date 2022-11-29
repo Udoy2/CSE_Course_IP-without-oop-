dda4ef6683999b62cc6d5559e36bf9fe344556b0
udoy2 <udoyrahman983@gmail.com>
2022-11-29 10:30:45 +0000

    lab9

--- lab9/inheritance.cpp
+++ lab9/inheritance.cpp
@@ -4,19 +4,18 @@ using namespace std;
 
 class Mobile {
 private:
-  int imeiNo=123224;
+  int imeiNo = 123224;
 
 protected:
-  string screen="gorilla";
+  string screen = "gorilla";
 
 public:
-  string model="65at saturn";
+  string model = "65at saturn";
 
   void s() { cout << "your phone screen type is :" << screen << endl; }
 
   void m() { cout << "your phone model type is : " << model << endl; }
 };
-
 
 class Samsung : private Mobile {
 public:
@@ -24,6 +23,10 @@ public:
 
   Samsung(string modell) {
     cout << "parameter constructor is : " << modell << endl;
+  }
+  void getData(void) {
+    s();
+    m();
   }
 };
 
@@ -38,39 +41,36 @@ public:
 
 class Iphone : protected Mobile {
 public:
-  Iphone() {
-       cout << "default constructor of Iphone" << endl; 
-    }
+  Iphone() { cout << "default constructor of Iphone" << endl; }
 
   Iphone(string modell) {
     cout << "parameter constructor is : " << modell << endl;
   }
+
+  void getData(void) {
+    s();
+    m();
+  }
 };
 
 int main(void) {
-    
-    Mobile mobile;
-    mobile.s();
-    mobile.m();
 
-    Samsung sam1;
-    Samsung sam2("galaxy s1") ;
+  Mobile mobile;
+  mobile.s();
+  mobile.m();
 
+  Samsung sam1;
+  Samsung sam2("galaxy s1");
+  sam1.getData();
 
-    Nokia nok1;
-    Nokia nok2("nok s1") ;
-    
-    Iphone iphn1;
-    Iphone iphn2("iphn s1");
-    
-    Nokia child;
-    child.s();
-    child.m();
+  Nokia nok1;
+  Nokia nok2("nok s1");
+  nok1.s();
+  nok1.m();
 
-    // Iphone child1;
-    // child1.s();
-    // child1.m();
+  Iphone iphn1;
+  Iphone iphn2("iphn s1");
+  iphn1.getData();
 
-     return 0; 
-     
-    }
+  return 0;
+}
